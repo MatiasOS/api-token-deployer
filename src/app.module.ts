3aabaa4ac8f687ed26e83a612d40b492784ca2be
loli.garciae <dgarciaelissondo@gmail.com>
2025-05-20 18:09:10 -0300

    fix: delete cors config and delete repeated code for config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,14 @@ import { validationSchema } from './env.validation';
 import { appConfig } from './config/app.config';
 import nebulaConfig from './config/nebula.config';
 
-void ConfigModule.forRoot({
-  envFilePath: ['.env.development.local', '.env.development'],
-});
-
 @Module({
   imports: [
+    NebulaModule,
     ConfigModule.forRoot({
       load: [nebulaConfig, appConfig],
       isGlobal: true,
       validationSchema,
     }),
-    NebulaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
