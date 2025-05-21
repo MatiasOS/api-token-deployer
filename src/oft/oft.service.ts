52c47a05470da538898a9eff981c9a9a19a73a4d
Mati OS <matias.ovejasmith@gmail.com>
2025-05-21 17:16:56 -0300

    oft: Add create service

---
+++ src/oft/oft.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@nestjs/common';
+import { CreateOftDto } from './dto/create-oft.dto';
+import { DeployerService } from './deployer/deployer.service';
+import { EndpointV2ProviderService } from './endpoint-v2-provider/endpoint-v2-provider.service';
+
+const blockchainMap: Record<'ethereum' | 'mantle' | 'arbitrum', string> = {
+  ethereum: 'sepolia',
+  mantle: 'mantleSepoliaTestnet',
+  arbitrum: 'arbitrumSepolia',
+};
+
+@Injectable()
+export class OftService {
+  constructor(
+    private readonly deployerService: DeployerService,
+    private readonly endpointV2Provider: EndpointV2ProviderService,
+  ) {}
+
+  async create(createOftDto: CreateOftDto): Promise<string[]> {
+    const { blockchain } = createOftDto;
+
+    const alTokeOFTArgs = {
+      name: createOftDto.name,
+      symbol: createOftDto.symbol,
+    };
+    const txs: Promise<string>[] = [];
+    blockchain.forEach((specificBlockchain) => {
+      const chain = blockchainMap[specificBlockchain] as
+        | 'sepolia'
+        | 'mantleSepoliaTestnet'
+        | 'arbitrumSepolia';
+      const endpointV2Address =
+        this.endpointV2Provider.endpointV2Address(chain);
+
+      const initialSupply = this.initialSupply({
+        chain: specificBlockchain,
+        distributions: createOftDto.distributions,
+      });
+      const txHash = this.deployerService.deploy({
+        chain,
+        contractName: 'AlTokeOFT',
+        deployArgs: {
+          ...alTokeOFTArgs,
+          endpointV2Address,
+          initialSupply: initialSupply,
+        },
+      });
+      txs.push(txHash);
+    });
+    const txsResolved = await Promise.all(txs);
+    return txsResolved;
+  }
+
+  initialSupply({
+    chain,
+    distributions,
+  }: {
+    chain: string;
+    distributions: {
+      blockchain: string;
+      address: string;
+      amount: string;
+    }[];
+  }): bigint {
+    const distributionsForChain = distributions.filter(
+      (distribution) => distribution.blockchain === chain,
+    );
+
+    const totalSupply = distributionsForChain.reduce((acc, distribution) => {
+      return acc + BigInt(distribution.amount);
+    }, 0n);
+
+    return totalSupply;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} oft`;
+  }
+}
