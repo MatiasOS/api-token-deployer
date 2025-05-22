3e98ce9a14758ebe0b301cf7e8b1954a423ae9bb
Mati OS <matias.ovejasmith@gmail.com>
2025-05-22 20:02:24 -0300

    App: Remove dead code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,32 +1,32 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NebulaModule } from './nebula/nebula.module';
 import { ConfigModule } from '@nestjs/config';
 import { validationSchema } from './env.validation';
 import { appConfig } from './config/app.config';
-import { TokenValueService } from './token-value/token-value.service';
 import { EstimatesModule } from './estimates/estimates.module';
 import nebulaConfig from './config/nebula.config';
 import coingreckoConfig from './config/coingrecko.config';
 import { OftModule } from './oft/oft.module';
 import { MerkleTreeModule } from './merkle-tree/merkle-tree.module';
+import { ChainModule } from './chain/chain.module';
+import { ContractsModule } from './contracts/contracts.module';
 import rpcConfig from './config/rpc.config';
-import wallets from './config/wallets.config'
+import wallets from './config/wallets.config';
+
 @Module({
   imports: [
-    NebulaModule,
     ConfigModule.forRoot({
       load: [nebulaConfig, appConfig, coingreckoConfig, rpcConfig, wallets],
       isGlobal: true,
-      validationSchema, // comment to prvent env validation
+      validationSchema, // comment to prevent env validation
       envFilePath: ['.env.development.local'],
     }),
+    NebulaModule,
     EstimatesModule,
     OftModule,
     MerkleTreeModule,
+    ChainModule,
+    ContractsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, TokenValueService],
 })
 export class AppModule {}
