4f2f3b14bbbcff49bb871f3cb81468c01029c496
Mati OS <matias.ovejasmith@gmail.com>
2025-05-23 00:14:42 -0300

    merkle: Add config endpoint and services

--- src/merkle-tree/merkle-tree.controller.ts
+++ src/merkle-tree/merkle-tree.controller.ts
@@ -2,10 +2,15 @@ import { Controller, Post, Body, Get, Query } from '@nestjs/common';
 import { MerkleTreeService } from './merkle-tree.service';
 import { CreateMerkleTreeDto } from './dto/create-merkle-tree.dto';
 import { DeployMerkletreeDto } from './dto/deploy-merkle-tree.dto';
+import { ConfigureMerkleTreeDto } from './dto/configure-merkle-tree.dto';
+import { OftService } from 'src/oft/oft.service';
 
 @Controller('merkle-tree')
 export class MerkleTreeController {
-  constructor(private readonly merkleTreeService: MerkleTreeService) {}
+  constructor(
+    private readonly merkleTreeService: MerkleTreeService,
+    private readonly oftService: OftService,
+  ) {}
 
   @Post()
   create(@Body() createMerkleTreeDto: CreateMerkleTreeDto) {
@@ -24,5 +29,15 @@ export class MerkleTreeController {
     blockchain: 'ethereum' | 'mantle' | 'arbitrum',
   ) {
     return this.merkleTreeService.getByTxHash({ txHash, blockchain });
+  }
+
+  @Post('/configure')
+  configure(@Body() configureMerkleTreeDto: ConfigureMerkleTreeDto) {
+    return this.oftService.transfer({
+      oftAddress: configureMerkleTreeDto.tokenAddress,
+      merkleTreeAddress: configureMerkleTreeDto.merkleTreeAddress,
+      blockchain: configureMerkleTreeDto.blockchain,
+      transferAmount: configureMerkleTreeDto.transferAmount,
+    });
   }
 }
--- src/merkle-tree/merkle-tree.module.ts
+++ src/merkle-tree/merkle-tree.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { MerkleTreeService } from './merkle-tree.service';
 import { MerkleTreeController } from './merkle-tree.controller';
 import { ChainModule } from 'src/chain/chain.module';
+import { OftService } from 'src/oft/oft.service';
+import { OftModule } from 'src/oft/oft.module';
+import { ContractsService } from 'src/contracts/contracts.service';
+import { ContractsModule } from 'src/contracts/contracts.module';
 
 @Module({
   controllers: [MerkleTreeController],
-  providers: [MerkleTreeService],
-  imports: [ChainModule],
+  providers: [MerkleTreeService, OftService, OftService, ContractsService],
+  imports: [ChainModule, OftModule, ContractsModule],
+  exports: [MerkleTreeService],
 })
 export class MerkleTreeModule {}
