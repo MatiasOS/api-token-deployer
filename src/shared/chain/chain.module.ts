3337166351cb87fa045c27de501886bb3c3b2b1a
Mati OS <matias.ovejasmith@gmail.com>
2025-05-25 17:57:31 -0300

    Shared: Move Chain and Contracts services

--- src/shared/chain/chain.module.ts
+++ src/shared/chain/chain.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChainService } from './chain.service';
 import { ConfigService } from '@nestjs/config';
-import { ContractsService } from 'src/contracts/contracts.service';
+import { ContractsService } from 'src/shared/contracts/contracts.service';
+import { ContractsModule } from '../contracts/contracts.module';
 
 @Module({
+  imports: [ContractsModule],
   providers: [ChainService, ConfigService, ContractsService],
   exports: [ChainService],
 })
