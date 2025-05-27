afcbc5aecfef35031f61610629a79c6f2a88ab7c
Mati OS <matias.ovejasmith@gmail.com>
2025-05-27 11:33:57 -0300

    Estimates: Remove dead code

--- src/estimates/estimates.service.ts
+++ src/estimates/estimates.service.ts
@@ -1,58 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import { CreateEstimateDto, EstimateItem } from './dto/create-estimate.dto';
 import { TokenValueService } from 'src/estimates/token-value.service';
-
-type GroupedEstimates = Record<string, EstimateItem[]>;
+import { SupportedChainId } from 'src/shared/types/chainId.types';
 
 export interface EstimateResultItem {
-  blockchain: string;
-  wiringEstimate: number;
-  contractCreationEstimate: number;
+  base: number;
+  chainId: SupportedChainId;
 }
 
 @Injectable()
 export class EstimatesService {
   constructor(private readonly coingreckoService: TokenValueService) {}
 
-  private groupByBlockchain(dto: CreateEstimateDto): GroupedEstimates {
-    return dto.estimates.reduce<GroupedEstimates>((acc, est) => {
-      const key = est.blockchain;
-      if (!acc[key]) acc[key] = [];
-      acc[key].push(est);
-      return acc;
-    }, {} as GroupedEstimates);
-  }
-
-  async estimateByBlockchain(
-    createEstimateDto: CreateEstimateDto,
-  ): Promise<EstimateResultItem[]> {
-    const grouped = this.groupByBlockchain(createEstimateDto);
-
-    const items = await Promise.all(
-      Object.entries(grouped).map(async ([blockchain, estimates]) => {
-        const tokenValue =
-          await this.coingreckoService.getTokenValue(blockchain);
-
-        const { wiringEstimate, contractCreationEstimate } = estimates.reduce(
-          (sums, est) => {
-            const base = tokenValue;
-            return {
-              wiringEstimate: sums.wiringEstimate + base * est.wiring,
-              contractCreationEstimate:
-                sums.contractCreationEstimate + base * est.contractCreation,
-            };
-          },
-          { wiringEstimate: 0, contractCreationEstimate: 0 },
-        );
-
-        return {
-          blockchain,
-          wiringEstimate,
-          contractCreationEstimate,
-        };
-      }),
-    );
-
-    return items;
+  estimateByBlockchain(chainId: SupportedChainId): EstimateResultItem {
+    return {
+      chainId,
+      base: 100,
+    };
   }
 }
